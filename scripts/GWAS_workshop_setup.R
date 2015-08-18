7865ee048bc13f0d3368790fa748f04eed8ebe8c
sirselim <miles.benton84@gmail.com>
2015-08-19 10:06:28 +1200

    updated scripts

--- scripts/GWAS_workshop_setup.R
+++ scripts/GWAS_workshop_setup.R
@@ -24,5 +24,57 @@ unzip(plink.zip, exdir = 'bin')
 # some mac systems seem to require the plink binary to be flagged as executable
 if(os.type == 'Darwin') system('chmod +x bin/./plink')
 
-cat('\n', 'PLINK extracted and installed in bin/', '\n')
-cat('\n', 'Done', '\n')
+cat('\n', 'PLINK extracted and installed in current working directory bin/', '\n')
+
+## set up R environment (packages) for workshop
+cat('\n', 'Checking packages and installing those required for this workshop...', '\n')
+
+# download, install and load required bioconductor packages
+# run this before base CRAN packages as Haplin depends on these
+cat("\n", "Installing required bioconductor packages...", "\n")
+#
+source("http://bioconductor.org/biocLite.R")
+biocLite("GenABEL")
+biocLite("GenABEL.data")
+biocLite("DatABEL")
+#
+cat("\n", "...Done...", "\n")
+
+# Install function for packages 
+# should allow detection of already installed packages   
+packages <- function(x){
+  x <- as.character(match.call()[[2]])
+  if (!require(x, character.only = TRUE)){
+    install.packages(pkgs = x, repos = "http://cran.r-project.org")
+    require(x, character.only = TRUE)
+  }
+}
+
+# use the above to download, install and load CRAN packages
+cat("\n", "Installing required CRAN packages...", "\n")
+#
+packages(devtools) # used to install packages from source and GitHub
+packages(MASS)
+packages(Haplin)
+packages(genetics)
+packages(LDheatmap)
+packages(RColorBrewer)
+#
+cat("\n", "...Done...", "\n")
+
+## required packages from GitHub repositories
+# NOTE: certain software will need to be installed to build the following
+# further instructions will be available in the GitHub README
+cat("\n", "Retrieving and installing Github pacakges...", "\n")
+# download, install and load qqman
+install_github("stephenturner/qqman", ref="dev")
+require("qqman")
+# download, install and load gdsfmt
+install_github("zhengxwen/gdsfmt")
+require("gdsfmt")
+# download, install and load SNPRelate
+install_github("zhengxwen/SNPRelate")
+require("SNPRelate")
+#
+cat("\n", "...Done. You are ready to start the workshop...", "\n")
+# END
