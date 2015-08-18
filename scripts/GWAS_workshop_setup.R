53a536b855ecfe276e9c9d9c1351550ce29aaf07
sirselim <miles.benton84@gmail.com>
2015-08-18 13:22:22 +1200

    updated scripts

--- scripts/GWAS_workshop_setup.R
+++ scripts/GWAS_workshop_setup.R
@@ -15,12 +15,14 @@ ifelse(os.type == 'Linux',
                             'bin/plink_win64.zip'),
               download.file('http://www.cog-genomics.org/static/bin/plink150805/plink_mac.zip', 
                             'bin/plink_mac.zip')))
-
 cat('\n', 'PLINK downloaded', '\n')
 
 # identify the zip file
 plink.zip <- list.files('bin/', pattern = '.zip', full.names = T)
 # unzip this file to the bin/ directory within the current working directory
 unzip(plink.zip, exdir = 'bin')
+# some mac systems seem to require the plink binary to be flagged as executable
+if(os.type == 'Darwin') system('chmod +x bin/./plink')
 
 cat('\n', 'PLINK extracted and installed in bin/', '\n')
+cat('\n', 'Done', '\n')
