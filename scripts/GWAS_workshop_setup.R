7801d9626f9bb91a8a7d4f7fda01d11e036e0cfd
sirselim <miles.benton84@gmail.com>
2015-08-18 12:14:23 +1200

    updated scripts

--- scripts/GWAS_workshop_setup.R
+++ scripts/GWAS_workshop_setup.R
@@ -7,12 +7,12 @@ unzip('example/example_data.zip', exdir = 'example')
 os.type <- Sys.info()['sysname']
 # download the latest OS specific version of plink 1.9
 ifelse(os.type == 'Linux', 
-       download.file('http://www.cog-genomics.org/static/bin/plink150618/plink_linux_x86_64.zip', 
+       download.file('http://www.cog-genomics.org/static/bin/plink150805/plink_linux_x86_64.zip', 
                      'bin/plink_linux_x86_64.zip'),
        ifelse(os.type == 'Windows', 
-              download.file('http://www.cog-genomics.org/static/bin/plink150618/plink_win64.zip', 
+              download.file('http://www.cog-genomics.org/static/bin/plink150805/plink_win64.zip', 
                             'bin/plink_win64.zip'),
-              download.file('http://www.cog-genomics.org/static/bin/plink150618/plink_mac.zip', 
+              download.file('http://www.cog-genomics.org/static/bin/plink150805/plink_mac.zip', 
                             'bin/plink_mac.zip')))
 
 # identify the zip file
